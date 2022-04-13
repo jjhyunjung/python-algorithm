9c3cdfc3a2fc47a2878ae103288f450aa6dba37b
Jihyun Jung <97021735+jjhyunjung@users.noreply.github.com>
2022-04-14 01:45:54 +0900

    14 Apr 2022

---
+++ 두 정수 사이의 합.py
@@ -0,0 +1,14 @@
+def solution(a, b):
+    answer = 0
+    
+    if a < b:
+        x = a
+        y = b
+    else:
+        x = b
+        y = a
+    
+    for i in range(x, y+1):
+        answer += i
+    
+    return answer
